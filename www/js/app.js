d7546bc63aa147cf5350fc2c6e4a570f078bf3e2
edenamch <edenamch@hotmail.com>
2017-08-24 10:31:18 -0500

    initial Commit structure

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+var altura =7;
+var base = 5;
+var area = base * altura;
+console.log("el area de mi rectangulo es igual: " + area)
+
+var pi= 3.14159
+var radio = 3
+var area = pi * (radio * radio);
+console.log("el area de mi circulo es igual: " + area)
+
+var altura =4;
+var base = 2;
+var area = (base * altura)/2;
+console.log("el area de mi triangulo es igual: " + area)
+
+var pi= 3.14159
+var radio = 3
+var area = ((3* pi)/4) * (radio * radio * radio);
+console.log("el volumen de mi esfera es igual: " + area)
